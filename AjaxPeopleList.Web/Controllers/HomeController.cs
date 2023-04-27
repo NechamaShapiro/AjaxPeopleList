c4c6cb745db1aee6181f7aeea5981291341a990c
Nechama Shapiro <nechamashapiro1234@gmail.com>
2023-04-27 17:40:09 -0400

    Add project files.

---
+++ AjaxPeopleList.Web/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using AjaxPeopleList.Data;
+using AjaxPeopleList.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AjaxPeopleList.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string _connectionString = @"Data Source=.\sqlexpress;Initial Catalog=PeopleDB;Integrated Security=true;";
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetPeople()
+        {
+            var repo = new PeopleRepository(_connectionString);
+            List<Person> people = repo.GetAll();
+            return Json(people);
+        }
+
+        [HttpPost]
+        public void AddPerson(Person person)
+        {
+            var repo = new PeopleRepository(_connectionString);
+            repo.Add(person);
+        }
+        [HttpPost]
+        public void DeletePerson(int id)
+        {
+            var repo = new PeopleRepository(_connectionString);
+            repo.Delete(id);
+        }
+        public IActionResult GetPersonToEdit(int id)
+        {
+            var repo = new PeopleRepository(_connectionString);
+            var person = repo.GetPersonById(id);
+            return Json(person);
+        }
+        [HttpPost]
+        public void UpdatePerson(Person person)
+        {
+            var repo = new PeopleRepository(_connectionString);
+            repo.Update(person);
+        }
+    }
+}
---
+++ AjaxPeopleList.Web/wwwroot/js/index.js
@@ -0,0 +1,69 @@
+﻿$(() => {
+    const modal = new bootstrap.Modal($('.modal')[0]);
+
+    //const saveNew = true;
+
+    function refreshTable() {
+        $("tbody").empty();
+        $.get('/home/getpeople', function (people) {
+            people.forEach(function (person) {
+                $("tbody").append(`<tr>
+            <td>${person.firstName}</td>
+            <td>${person.lastName}</td>
+            <td>${person.age}</td>
+            <td><button class="btn btn-warning edit-person" id="${person.id}">Edit</button></td>
+            <td><button class="btn btn-danger delete-person" id="${person.id}">Delete</button></td>
+</tr>`)
+            });
+        });
+    }
+
+    refreshTable();
+
+    $("#add-person").on('click', function () {
+        $("#firstName").val('');
+        $("#lastName").val('');
+        $("#age").val('');
+        modal.show();
+        saveNew = true;
+    });
+
+    $("#save-person").on('click', function () {
+        const firstName = $("#firstName").val();
+        const lastName = $("#lastName").val();
+        const age = $("#age").val();
+        const id = $("#id").val();
+        if (saveNew) {
+            $.post('/home/addperson', { firstName, lastName, age }, function () {
+                modal.hide();
+                refreshTable();
+            });
+        }
+        else {
+            $.post('/home/updateperson', { firstName, lastName, age, id }, function () {
+                modal.hide();
+                refreshTable();
+            });
+        }
+
+    });
+    $("tbody").on('click', '.edit-person', function () {
+        const id = $(this).attr('id');
+        saveNew = false;
+        $.get('/home/getpersontoedit', { id }, function (person) {
+            $("#firstName").val(person.firstName);
+            $("#lastName").val(person.lastName);
+            $("#age").val(person.age);
+            $("#id").val(person.id);
+            modal.show();
+        });
+        
+        
+    });
+    $("tbody").on('click', '.delete-person', function () {
+        const id = $(this).attr('id');
+        $.post('/home/deleteperson', { id }, function () {
+            refreshTable();
+        });
+    });
+});
